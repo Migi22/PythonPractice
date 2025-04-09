fae7131f31dcadf8fdd60fd2d6070021ca5883d4
Lance <miguel2002622@gmail.com>
2025-04-09 16:19:10 +0800

    Message in main.py and to be continue for python_variables.py in next free time

---
+++ main.py
@@ -0,0 +1,4 @@
+# Fixing my python skills
+print("Hello Marianne!")
+print("My skills are not that good. But I'm working on it \n*wink* *wink* ;3")
+
---
+++ python_variables.py
@@ -0,0 +1,22 @@
+# Variables
+
+# Strings
+first_name = "Lance"
+food = "Pizza"
+email = "lance@fakeEmail.com"
+
+print(f"Hello! I'm {first_name} and I like {food}. \n"
+      f"Contact me @ my email '{email}' \n\n*wink* *wink* ;3 \n")
+
+# Integers
+
+age = 22
+quantity = 2
+
+print(f"Currently I am {age} year's old.\n"
+      f"I have {quantity} cats in my house.")
+
+# Float
+price = 69.69
+
+print(f"I have bought a food worth a \u20B1{price} pesos, Amazing! isn't it?")
