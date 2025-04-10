e149274592185c7d3c95f5baf544652592310632
Lance <miguel2002622@gmail.com>
2025-04-10 13:53:18 +0800

    Learned Data types

--- python_variables.py
+++ python_variables.py
@@ -18,5 +18,33 @@ print(f"Currently I am {age} year's old.\n"
 
 # Float
 price = 69.69
+gpa = 3.2
+distance = 5.5
 
-print(f"I have bought a food worth a \u20B1{price} pesos, Amazing! isn't it?")
+print(f"I have bought a food worth a \u20B1{price} pesos, Amazing! isn't it?\n"
+      f"Your gpa is: {gpa}\n"
+      f"We run a distance of {distance}\n")
+
+# Boolean
+
+is_student = True
+for_sale = False
+is_online = True
+
+print(f"Are you a student? {is_student}\n")
+
+if is_student:
+      print("You are a student\n")
+else:
+      print("You are NOT a student\n")
+
+if for_sale:
+      print("That item is for sale")
+else:
+      print("That is NOT available")
+
+if is_online:
+      print("You are online!")
+else:
+      print("You are not online!")
+
